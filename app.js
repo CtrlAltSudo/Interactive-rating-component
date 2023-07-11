91bc408902ed24d55e6dc360f4e5cadc9315f2b2
Damien White <damienkwhite@gmail.com>
2023-07-11 07:23:18 +0100

    finally figured out the hover state issue, added some mobile friendliness

--- app.js
+++ app.js
@@ -11,30 +11,30 @@ rating.forEach(function(list) { // Iterates over all the li HTML tags
     rating.forEach(function(sel) { // Iterates over the rating 
       if (sel !== list) { // Checks if selection is not the same as initial click
         sel.classList.remove("selection"); // removes selection CSS class 
-      } else 
+      } else {
       selectedValue = list.innerText; // stores the selected value in a variable
       thankYou.innerText = selectedValue + " ";
       list.classList.toggle("selection"); // Adds or removes selection class
       console.log("Element clicked!"); // Used to help me check if code was working
+      }
     });
   });
-  list.addEventListener('mouseover', function() {
+  list.addEventListener('mouseover', function() { // checks for selection on mouse over and either removes or adds a css class
     if (!list.classList.contains("selection")) {
-      list.classList.add("ul.li:hover");
+      list.classList.add("hover");
+    } else if(list.classList.contains("selection")) {
+      list.classList.remove("hover");
     }
   });
 
-  list.addEventListener('mouseout', function() {
-    if (!list.classList.contains("selection")) {
-      list.classList.remove("ul.li:hover");
+  list.addEventListener('click', function() { // also checks for selection but on mouse click and either removes or adds a css class
+    if (list.classList.contains("selection")) {
+      list.classList.remove("hover");
+    } else if (!Ílist.classList.contains("selection")) {
+      list.classList.add("hover");
     }
   });
 });
-
-
-
-
-
 
 
 form.addEventListener("submit", function (event) {
